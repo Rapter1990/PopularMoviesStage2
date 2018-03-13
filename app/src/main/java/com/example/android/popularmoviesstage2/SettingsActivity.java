ad9450ed6380d5d964ef7208ba22d9357c9a44ff
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-03-13 12:56:49 +0300

    Commited Changes

--- app/src/main/java/com/example/android/popularmoviesstage2/SettingsActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage2/SettingsActivity.java
@@ -11,6 +11,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     // TODO : 114 ) Creating onOptionsItemSelected method
